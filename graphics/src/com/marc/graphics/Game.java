6ae51854bf57376cb58c870f44ce00c243ee4330
marcfreir <marcfreir@outlook.com>
2020-07-15 18:52:27 -0200

    Updated Game.java and Added Game.txt to sandbox

--- graphics/src/com/marc/graphics/Game.java
+++ graphics/src/com/marc/graphics/Game.java
@@ -1,7 +1,11 @@
 package com.marc.graphics;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 /**
@@ -19,12 +23,15 @@ public class Game extends Canvas implements Runnable
     private final int WIDTH = 160;
     private final int HEIGHT = 120;
     private final int SCALE = 3;
+
+    private BufferedImage image;
     
     //Constructor
     public Game()
     {
-        setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
         initFrame();
+        image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
     }
 
     public void initFrame()
@@ -47,10 +54,13 @@ public class Game extends Canvas implements Runnable
         thread.start();
     }
 
+
+
     public synchronized void stop()
     {
-
+        //something
     }
+
 
     public static void main(String[] args)
     {
@@ -58,12 +68,59 @@ public class Game extends Canvas implements Runnable
         game.start();
     }
 
+    public void beatUpdate()
+    {
+        //Something
+    }
+
+    public void render()
+    {
+        //To organize complex memory on the Canvas
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null)
+        {
+            this.createBufferStrategy(3);
+            return;
+        }
+        //To draw onto Canvas
+        Graphics graphics = image.getGraphics();
+        graphics.setColor(new Color(19,19,19));
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+        graphics = bs.getDrawGraphics();
+        graphics.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE,null);
+        bs.show();
+    }
+
     @Override
     public void run()
     {
+        long lastTime = System.nanoTime();
+        double amountOfBeats = 60.0;
+        double nanosecPerAmountOfBeats = 1000000000 / amountOfBeats;
+        double delta = 0;
+        int framesPerSecond = 0;
+        double timer = System.currentTimeMillis();
         while (isRunning)
         {
-            System.out.println("My game is running!...");
+            long now = System.nanoTime();
+            delta+= (now - lastTime) / nanosecPerAmountOfBeats;
+            lastTime = now;
+
+            if (delta >= 1)
+            {
+                beatUpdate();
+                render();
+                framesPerSecond++;
+                delta--;
+            }
+
+            if ((System.currentTimeMillis() - timer) >= 1000)
+            {
+                System.out.println("FPS: " + framesPerSecond);
+                framesPerSecond = 0;
+                timer += 1000;
+            }
+            /**System.out.println("My game is running!...");*/
         }
 
     }
