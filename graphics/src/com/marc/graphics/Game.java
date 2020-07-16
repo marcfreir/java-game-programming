d66662bef7eab00c94cfc5706f23ee7a70647f84
marcfreir <marcfreir@outlook.com>
2020-07-16 01:24:13 -0200

    Updated Game.java

--- graphics/src/com/marc/graphics/Game.java
+++ graphics/src/com/marc/graphics/Game.java
@@ -3,62 +3,71 @@ package com.marc.graphics;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
+
 /**
  * Graphics
  */
-public class Game extends Canvas implements Runnable
-{
+public class Game extends Canvas implements Runnable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
     public static JFrame frame;
 
-    private Thread thread;
+    private transient Thread thread;
 
     private boolean isRunning = true;
 
-    private final int WIDTH = 160;
-    private final int HEIGHT = 120;
-    private final int SCALE = 3;
+    private static final int WIDTH = 160;
+    private static final int HEIGHT = 120;
+    private static final int SCALE = 4;
 
-    private BufferedImage image;
-    
-    //Constructor
-    public Game()
-    {
-        setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+    private transient BufferedImage image;
+
+    // Constructor
+    public Game() {
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
         initFrame();
-        image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
     }
 
-    public void initFrame()
-    {
+    public void initFrame() {
         frame = new JFrame("Game #01");
         frame.add(this);
         frame.setResizable(false);
         frame.pack();
-        //Centered window
+        // Centered window
         frame.setLocationRelativeTo(null);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
-    public synchronized void start()
-    {
+    public synchronized void start() {
         thread = new Thread(this);
         isRunning = true;
         thread.start();
     }
 
-
-
-    public synchronized void stop()
-    {
-        //something
+    public synchronized void stop() {
+        isRunning = false;
+        try
+        {
+            thread.join();
+        }
+        catch (InterruptedException exception)
+        {
+            exception.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
     }
 
 
@@ -86,6 +95,13 @@ public class Game extends Canvas implements Runnable
         Graphics graphics = image.getGraphics();
         graphics.setColor(new Color(19,19,19));
         graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+        /**graphics.setColor(Color.MAGENTA);
+        graphics.fillRect(20, 20, 80, 80);*/
+        graphics.setColor(Color.WHITE);
+        graphics.setFont(new Font("Arial", Font.BOLD, 16));
+        graphics.drawString("Hello Friend!", 15, 15);
+
         graphics = bs.getDrawGraphics();
         graphics.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE,null);
         bs.show();
@@ -122,6 +138,7 @@ public class Game extends Canvas implements Runnable
             }
             /**System.out.println("My game is running!...");*/
         }
+        stop();
 
     }
 
