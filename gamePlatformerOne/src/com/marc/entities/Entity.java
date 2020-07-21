11b142ce39fb58807cf9aaf668200df1467efb3b
marcfreir <marcfreir@outlook.com>
2020-07-21 03:20:38 -0200

    Added Entity.java and Spritesheet.java

---
+++ gamePlatformerOne/src/com/marc/entities/Entity.java
@@ -0,0 +1,45 @@
+package com.marc.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Entity
+{
+	private int entityX;
+	private int entityY;
+	private int entityWidth;
+	private int entityHeight;
+	
+	//
+	private BufferedImage sprite;
+	
+	//Constructor
+	public Entity(int entityX, int entityY, int entityWidth, int entityHeight, BufferedImage sprite)
+	{
+		this.entityX = entityX;
+		this.entityY = entityY;
+		this.entityWidth = entityWidth;
+		this.entityHeight = entityHeight;
+		this.sprite = sprite;
+	}
+	
+	//Getters
+	public int getEntityX()
+	{
+		return this.entityX;
+	}
+	
+	public int getEntityY()
+	{
+		return this.entityY;
+	}
+	
+	public int getEntityWidth()
+	{
+		return this.entityWidth;
+	}
+	
+	public int getEntityHeight()
+	{
+		return this.entityHeight;
+	}
+}
---
+++ gamePlatformerOne/src/com/marc/graphics/Spritesheet.java
@@ -0,0 +1,29 @@
+package com.marc.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Spritesheet
+{
+	private BufferedImage spritesheet;
+	
+	//Constructor
+	public Spritesheet(String path)
+	{
+		try
+		{
+			spritesheet = ImageIO.read(getClass().getResource(path));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public BufferedImage getSprite(int axisXSpritesheet, int axisYSpritesheet, int widthSpritesheet, int heightSpritesheet)
+	{
+		return spritesheet.getSubimage(axisXSpritesheet, axisYSpritesheet, widthSpritesheet, heightSpritesheet);
+	}
+}
